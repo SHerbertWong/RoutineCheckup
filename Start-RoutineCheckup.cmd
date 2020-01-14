8fa47d0afb9bd5b93472e02bb4867063cd1fa49c
SHerbertWong <SHerbertWong@users.noreply.github.com>
2020-01-14 16:13:07 +0800

    Commit 275D82690B264978C324F962783FF49D53140DE8
    - Workarounds for "%"-signs in URLs

--- Start-RoutineCheckup.cmd
+++ Start-RoutineCheckup.cmd
@@ -6,14 +6,14 @@ SET "CmdScriptPath=%~f0"
 SET "BootStrapperUri=https://raw.githubusercontent.com/SHerbertWong/RoutineCheckup/master/Start-RoutineCheckupBootstrapping.ps1"
 SET "BootStrapperFunctionName=Start-RoutineCheckupBootStrapping"
 SET "BootStrapperFileName=%BootStrapperFunctionName%.ps1"
-SET "WgetUri=https://onedrive.live.com/download?cid=63B88D4120E75E9C&resid=63B88D4120E75E9C%211335&authkey=AB1YzjkM8DwpMOc"
+SET "WgetUri=https://onedrive.live.com/download?cid=63B88D4120E75E9C&resid=63B88D4120E75E9C%%211335&authkey=AB1YzjkM8DwpMOc"
 SET "WgetFileName=wget.exe"
 
 FOR /F "tokens=* USEBACKQ" %%f IN (`POWERSHELL -ExecutionPolicy Bypass -Command "Join-Path -Path $env:TEMP -ChildPath ([IO.Path]::GetFileNameWithoutExtension([IO.Path]::GetRandomFileName()))"`) DO (SET "RoutineCheckupRootPath=%%f")
 MD "%RoutineCheckupRootPath%"
 SET "PATH=%RoutineCheckupRootPath%;%PATH%"
 POWERSHELL -ExecutionPolicy Bypass -Command ^
-	"Write-Host -Object 'Downloading %WgetFileName%... ' -NoNewline; (New-Object -TypeName Net.WebClient).DownloadFile('%WgetUri%', (Join-Path -Path '%RoutineCheckupRootPath%' -ChildPath '%WgetFileName%')); Write-Host -Object 'Done!'"
+	"Write-Host -Object 'Downloading %WgetFileName%... ' -NoNewline; (New-Object -TypeName Net.WebClient).DownloadFile(($env:WgetUri), (Join-Path -Path '%RoutineCheckupRootPath%' -ChildPath '%WgetFileName%')); Write-Host -Object 'Done!'"
 POWERSHELL -ExecutionPolicy Bypass -Command ^
 	"Write-Host -Object 'Downloading %BootStrapperFileName%... ' -NoNewline; (New-Object -TypeName Net.WebClient).DownloadFile('%BootStrapperUri%', (Join-Path -Path '%RoutineCheckupRootPath%' -ChildPath '%BootStrapperFileName%')); Write-Host -Object 'Done!'"
 START POWERSHELL -ExecutionPolicy Bypass -Command ^
